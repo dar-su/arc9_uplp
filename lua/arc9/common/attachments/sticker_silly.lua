1d5c2cd092f8cfea3306fada490efc1ff1efae95
Darsu <95997959+dar-su@users.noreply.github.com>
2022-12-27 16:38:37 +0700

    silly stickers

---
+++ lua/arc9/common/attachments/sticker_silly.lua
@@ -0,0 +1,13 @@
+ATT.PrintName = [[Silly]]
+ATT.Description = [[This is how u look for using this...
+
+Sticker included in Low poly pack.]]
+ATT.Icon = Material("stickers/silly")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+-- ATT.Folder = "Stickers" -- Change this to point what part of the weapon it goes.
+
+ATT.Category = {"stickers"} -- Change this to point what weapon it is for.
+ATT.StickerMaterial = "stickers/silly" -- Add a custom sticker image here.
