2bc48d12ee82c161863c3387a3692ebcc89bc925
Darsu <95997959+dar-su@users.noreply.github.com>
2023-10-18 17:30:43 +0700

    Unfixedcrayfish rig included

---
+++ lua/autorun/unfixedcrayfishchands.lua
@@ -0,0 +1,4 @@
+if SERVER then AddCSLuaFile() end
+
+player_manager.AddValidModel("UnfixedCrayfish Low-Poly Rig", "models/player/darsu/c_unfixedcrayfish_rig.mdl")
+player_manager.AddValidHands( "UnfixedCrayfish Low-Poly Rig", "models/player/darsu/c_unfixedcrayfish_rig.mdl", 0, "00000000" )
