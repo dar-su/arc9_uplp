f62d780308f41bf967f6cf74eccbd7efe1101788
TheOnly8Z <888888zombies@gmail.com>
2023-11-22 16:09:25 -0800

    spawnmenu icons (dev branch only)

---
+++ lua/arc9/shared/sh_uplp.lua
@@ -0,0 +1,4 @@
+-- As of 2023-11-12, this feature is only available on dev branch.
+-- Won't break anything on release branch though.
+list.Set("ContentCategoryIcons", "ARC9 - Poly Arms", "uplp_16.png")
+list.Set("ContentCategoryIcons", "ARC9 - Poly Arms Attachments", "uplp_16.png")
