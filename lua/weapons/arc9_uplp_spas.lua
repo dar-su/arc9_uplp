710e9eaa6a08f14790034efcd11a7788d1251012
Fesiug <publicfesiug@outlook.com>
2024-03-18 05:20:58 -0400

    SPAS more tweaks

--- lua/weapons/arc9_uplp_spas.lua
+++ lua/weapons/arc9_uplp_spas.lua
@@ -66,7 +66,7 @@ SWEP.CamQCA = 3
 SWEP.CamOffsetAng = Angle(0, 0, 90)
 
 ---- View & Worldmodel
-SWEP.ViewModel = "models/weapons/arc9/c_uplp_spas-30.mdl"
+SWEP.ViewModel = "models/weapons/arc9/c_uplp_spas-41.mdl"
 SWEP.WorldModel = "models/weapons/arc9/w_uplp_spas.mdl"
 
 SWEP.MirrorVMWM = true
@@ -471,7 +471,7 @@ SWEP.Hook_TranslateAnimation = function(swep, anim)
     -- Pump-action mode suppresses the empty suffix
     if insemi and swep:GetEmptyReload() and anim == "reload_start" then
         swep:SetEmptyReload(false)
-        return "reload_start_empty"
+        return "reload_start_empty_pumpy"
     end
 
     if insemi and anim == "fire" then
@@ -582,10 +582,25 @@ SWEP.Animations = {
     },
 
     ["reload_start_empty"] = {
+        Source = "reload_start_empty",
+        RestoreAmmo = 1,
+		MinProgress = 0.2,
+        Mult = 0.85,
+        EventTable = {
+            { s = UTCrattle, t = 0 / 30, c = ca, v = 0.8 },
+            { s = pathUT .. "breechload.ogg", t = 0 / 30, v = 0.6 },
+            { s = pathUT .. "forearm_forward.ogg", t = 26 / 30, v = 0.6 },
+            { s = "uplp_urban_temp/ar15/" .. "boltdrop.ogg", t = 27 / 30, v = 0.6 },
+			
+            { s = pathUTC .. "rattle_b2i_rifle.ogg", t = 30 / 30, c = ca, v = 1 },
+            { s = pathUTC .. "grab-polymer.ogg", t = 49 / 30, c = ca, v = 0.4 },
+        },
+    },
+
+    ["reload_start_empty_pumpy"] = {
         Source = "reload_start_empty_pumpy",
         RestoreAmmo = 1,
-		MinProgress = 0.25,
-		RefillProgress = 0.0,
+		MinProgress = 0.2,
         Mult = 0.85,
         EjectAt = 31 / 30,
         EventTable = {
@@ -593,6 +608,9 @@ SWEP.Animations = {
             { s = ShellInsert, t = 14 / 30, v = 0.6 },
             { s = pathUT .. "forearm_back.ogg", t = 26 / 30, v = 0.6 },
             { s = pathUT .. "forearm_forward.ogg", t = 33 / 30, v = 0.6 },
+			
+            { s = pathUTC .. "rattle_b2i_rifle.ogg", t = 30 / 30, c = ca, v = 1 },
+            { s = pathUTC .. "grab-polymer.ogg", t = 49 / 30, c = ca, v = 0.4 },
         },
     },
 
@@ -600,7 +618,7 @@ SWEP.Animations = {
         Source = "reload_insert",
         MinProgress = 0.334,
         EventTable = {
-            { s = ShellInsert, t = 3 / 30, v = 0.6 },
+            { s = ShellInsert, t = 1 / 30, v = 0.6 },
         },
     },
 
