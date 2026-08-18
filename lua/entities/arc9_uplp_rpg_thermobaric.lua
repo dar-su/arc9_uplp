7a644ebca4228898151575ecf20b5f10741c5a91
TheOnly8Z <888888zombies@gmail.com>
2026-08-18 17:57:41 -0400

    thermobaric

--- lua/entities/arc9_uplp_rpg_thermobaric.lua
+++ lua/entities/arc9_uplp_rpg_thermobaric.lua
@@ -24,28 +24,51 @@ ENT.SmokeTrail = false
 ENT.RocketTrail = true
 
 ENT.FlareColor = Color(255, 120, 0)
-ENT.Radius = 512
+
+ENT.Radius = 300
 
 local pathd = "uplp_urban_temp/m203/"
 local path = "uplp_urban_temp/common/"
 ENT.ExplosionSounds = {path .. "explosion-rpg-01.ogg", path .. "explosion-rpg-02.ogg", path .. "explosion-rpg-03.ogg", path .. "explosion-rpg-04.ogg", path .. "explosion-rpg-05.ogg", path .. "explosion-rpg-06.ogg"}
 ENT.DebrisSounds = {pathd .. "debris-01.ogg", pathd .. "debris-02.ogg", pathd .. "debris-03.ogg", pathd .. "debris-04.ogg", pathd .. "debris-05.ogg"}
 
+local trace_dirs =
+{
+    Vector(512, 0, 0),
+    Vector(0, 512, 0),
+    Vector(-512, 0, 0),
+    Vector(0, -512, 0),
+    Vector(0, 0, 512),
+    Vector(0, 0, 1024),
+}
+
 function ENT:Detonate(hitEnt, data)
     local attacker = self.Attacker or self:GetOwner()
     local dir = self:GetVelocity():GetNormalized()
-    local src = self:GetPos() - dir * 64
+    local src = self:GetPos() - dir * 32
+
+    local strength = 0
+    for _, dir in ipairs(trace_dirs) do
+        local tr = util.TraceLine({
+            start = src,
+            endpos = src + dir,
+            mask = MASK_SOLID_BRUSHONLY,
+            filter = self,
+        })
+        strength = strength + (tr.HitSky and 1 or tr.Fraction)
+    end
+
+    local d = 1 - strength / #trace_dirs
+    local radius = Lerp(d, self.Radius, self.Radius * 1.5)
 
     local dmg = DamageInfo()
     dmg:SetAttacker(attacker)
-    dmg:SetDamageType(DMG_BLAST + DMG_BURN)
+    dmg:SetDamageType(DMG_BLAST + DMG_BURN + DMG_AIRBOAT)
     dmg:SetInflictor(self)
     dmg:SetDamageForce(self:GetForward() * 5000)
     dmg:SetDamagePosition(src)
-    dmg:SetDamage(100)
-    util.BlastDamageInfo(dmg, self:GetPos(), self.Radius)
-    dmg:SetDamageType(DMG_BLAST + DMG_AIRBOAT)
-    util.BlastDamageInfo(dmg, self:GetPos(), self.Radius / 3)
+    dmg:SetDamage(Lerp(d, 100, 400))
+    util.BlastDamageInfo(dmg, self:GetPos(), radius)
 
     local fx = EffectData()
     fx:SetOrigin(self:GetPos())
@@ -64,7 +87,15 @@ function ENT:Detonate(hitEnt, data)
         -- explosion_grenade_fas2
 
         self:EmitSound(self.ExplosionSounds[math.random(1,#self.ExplosionSounds)], 125, 100, 1, CHAN_AUTO, _, _, ARC9.EveryoneRecipientFilter)
-        ParticleEffect("explosion_m79", self:GetPos(), dir:Angle())
+        ParticleEffect("explosion_HE_m79_fas2", self:GetPos(), dir:Angle())
+
+        if d >= 0.5 then
+            ParticleEffect("grenade_explosion_01", self:GetPos(), dir:Angle())
+            ParticleEffect("striderbuster_attach_flash", self:GetPos(), dir:Angle())
+            ParticleEffect("striderbuster_break_b", self:GetPos(), dir:Angle())
+            ParticleEffect("striderbuster_break_explode", self:GetPos(), dir:Angle())
+            ParticleEffect("striderbuster_explode_dummy_core", self:GetPos(), dir:Angle())
+        end
     end
 
     util.ScreenShake(self:GetPos(), 25, 4, 0.75, self.Radius * 3)
